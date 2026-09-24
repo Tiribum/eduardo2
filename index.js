27daa9c80603529fe4a14649140d123d850d3fd3
aaaaaa <84544156+Tiribum@users.noreply.github.com>
2026-09-23 21:38:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ statusBot.once('ready', () => {
     console.log(`[Status Bot] Online e conectado como ${statusBot.user.tag}`);
     
     // Altere 'Nome da sua Atividade' para o que deseja exibir no perfil
-    statusBot.user.setActivity('teste', { 
+    statusBot.user.setActivity('Eduardo agora esta operando 100%', { 
         type: ActivityType.Playing 
     });
 });
